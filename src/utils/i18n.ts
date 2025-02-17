8e1818c1c02cefe3a7c18a23802fb3327312f24f
Nikesh Maharjan <nikeshmhr@gmail.com>
2025-02-17 21:09:37 +0400

    remove chinese lang

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,8 +1,7 @@
 import { MISC } from "@/config";
 
 export const languages = {
-  en: "EN",
-  zh: "中",
+  en: "en",
 };
 
 export const defaultLang: Lang = "en";
@@ -33,32 +32,8 @@ export const ui = {
     "post.oldPostWarningMsg": `Last update over ${MISC.dateTag.daysToBeRed} days ago`,
     "post.license": "Licensed under",
     "post.notSupportedLang": "Language not supported",
-    "post.notSupportedLangDescription": "Sorry, your language is unavailable for this post.",
-  },
-  zh: {
-    "nav.home": "首页",
-    "nav.tags": "标签",
-    "nav.tags.description": "这里是本站的标签索引，您可以在这里浏览并快速找到与您感兴趣主题相关的文章和内容。",
-    "nav.about": "关于",
-    "search.placeholder.firstPart": "在 ",
-    "search.placeholder.secondPart.post": " 篇文章中搜索...",
-    "search.placeholder.secondPart.tag": " 个标签中搜索...",
-    "search.noResults": "没有找到结果",
-    "postsWithTag.firstPart": "带有",
-    "postsWithTag.secondPart": "标签的文章",
-    "postsWithTag.description.firstPart": "这里是所有带有",
-    "postsWithTag.description.secondPart": "标签的文章。",
-    toc: "目录",
-    pageNotFound: "未找到此页面",
-    pageNotFoundDescription: "抱歉，您请求的页面未找到。请检查网址或返回主页。",
-    goBackHome: "返回首页",
-    publishedAt: "发表于",
-    updatedAt: "更新于",
-    "post.newlyUpdatedMsg": `更新于 ${MISC.dateTag.daysToBeGreen} 日内`,
-    "post.oldPostWarningMsg": `更新于 ${MISC.dateTag.daysToBeRed} 日前`,
-    "post.license": "许可证",
-    "post.notSupportedLang": "语言暂不支持",
-    "post.notSupportedLangDescription": "抱歉，此文章暂不支持您的语言。",
+    "post.notSupportedLangDescription":
+      "Sorry, your language is unavailable for this post.",
   },
 } as const;
 
